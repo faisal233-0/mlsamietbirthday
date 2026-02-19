fdaa3b3aec808293241acf966d88cd49e721b329
Mo faisal <faisalshaikh47757@gmail.com>
2026-02-19 23:40:13 +0530

    solved issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "Faisal-tagger";
+import { componentTagger } from "Lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
